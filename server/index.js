37378c687543c49de45d7dd0af7bdff39f0c7fcd
Mufeed k <mufeedmangalad@gmail.com>
2023-12-09 17:04:54 +0530

    user-registration-completed

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
 import express from "express";
-import bodyParser from "bodyParser";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
-import morgon from "morgon";
+import morgon from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import { register } from "./controllerse/auth.js";
+import authRoutes from "./routes/auth.js";
 /* CONFIGURATIONS */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.configur();
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOrginResourcePolicy({ policy: "cross-orgin" }));
+// app.use(helmet({crossOriginResourcePolicy:{ policy: "cross-orgin" }}));
 app.use(morgon("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -34,5 +36,18 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+/*ROUTES WITH FILE */
+app.use("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port:${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
